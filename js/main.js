80e16a1038e69d0e5f0d786041b6ca64beba10f6
Johannes1803 <Johannes.Birk11@web.de>
2016-05-15 13:00:30 +0200

    Add Functionality: Upon Clicking the current cat is changed and the respective cat image displayed
    When clikcing on a cat element in the list the Modell is updated and the correct image displayed.

--- js/main.js
+++ js/main.js
@@ -11,42 +11,71 @@ $(function(){
 		img_url: "blabla"},
 		]
 	};
+	// list of cat elements stored as variable in global scope
+	var cats = model.cats;
 
 
+	// view function for the list of cats
 	var view_list = {
 		init: function(cats){
 			// append each element in list to the DOM
 			for (i=0; i < cats.length; i++){
 				add_cat = cats[i];
 				$("#catlist").append("<li id='" + add_cat.name +"'>" + add_cat.name + "</li>");
+			};
+			// Upon click on list element: change current_cat in data, rerender image
+			$("#catlist").click(function(event) {
+    			var text = $(event.target).text();
+    			console.log(text);
+    			// call octopus to update the model
+    			octopus.update_current_cat(text);
+    			octopus.image_render();
+			});
 
-			}
+
 
 		}
 	};
+
+	// view function for the selected cat image
 	var view_image = {
-		render: function(current_cat){
+		init: function(current_cat){
+					$(".cat_pic").empty();
 					$(".cat_pic").append("<div id='" + current_cat.name + "'>" + current_cat.img_url +"</div>");
-				}
+		}
 	};
 
 	// octopus
 	var octopus = {
-		initial_render: view_list.init(model.cats),
 		// call the view_list.init function, pass in the list of cats
+		init_list: function(){
+			view_list.init(cats)
+		},
+
+		// render the image
 		image_render: function(){
-			var cats = model.cats;
 			var current_cat = model.current_cat;
 			for (i=0; i < cats.length; i++){
 				var add_cat = cats[i];
 				if (add_cat.name == current_cat){
-					console.log(add_cat);
-					view_image.render(add_cat);
+					// call the view_image.init function, pass in the currently selected
+					// cat object as argument
+					//console.log(add_cat);
+					view_image.init(add_cat);
 				}
 			}
 
-		} 
-		// call the view_image.render function, pass in the array of cats and the current cat
+		},
+		update_current_cat: function(current_cat){
+			model.current_cat = current_cat;
+		}
+		/*click_list: function(){
+			$("#")click(function(){
+				console.log("Hurray!")
+			})*/
+
+
 	};
-octopus.image_render();
+	octopus.image_render();
+	octopus.init_list();
 });
